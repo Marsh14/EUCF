d778fb97c3452c02dfed444e66d4bf1938fa5a5e
Benjamin Quintero <bquinteromdc@gmail.com>
2025-06-28 16:06:22 -0400

    first actual real commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { relative } from "path";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <div className="w-full h-[160px] bg-gradient-to-r from-[#ffffff] to-[#B49758]">
+        <Image
+          src = "/1._Primary_Shield.png"
+          alt = "EUCF Logo Top Bar"
+          width={100}
+          height={120}
+        />
+      </div>
     </div>
   );
 }
